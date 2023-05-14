e2b6d9f213fa07c24dfc1aab6ac4f73603318564
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-05-14 13:07:00 -0300

    initial commit new repository

---
+++ src/main/java/com/portfolio/ls/Interface/IPersonaService.java
@@ -0,0 +1,17 @@
+package com.portfolio.ls.Interface;
+
+import com.portfolio.ls.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+
+    
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}

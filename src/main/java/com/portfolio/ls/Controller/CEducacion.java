16f03af0e7de05ea592c41d7f21f71d8e849ef67
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-05-15 18:44:11 -0300

    final commit

--- src/main/java/com/portfolio/ls/Controller/CEducacion.java
+++ src/main/java/com/portfolio/ls/Controller/CEducacion.java
@@ -19,10 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author lor_b
- */
 @RestController
 @RequestMapping("/educacion")
 //@CrossOrigin(origins = "http://localhost:4200")

99d7a6e64e9b27cd540d445bd3b4f97ea959471c
Lorenzo Sepulveda <115026126+LorenzoSepulveda@users.noreply.github.com>
2023-06-23 16:50:09 -0300

    Update CExperiencia.java

--- src/main/java/com/portfolio/ls/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/ls/Controller/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app/")
+@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app")
 
 public class CExperiencia {
 

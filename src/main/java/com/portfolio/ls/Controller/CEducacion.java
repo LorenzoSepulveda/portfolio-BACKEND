ab5893c4ba0f434afe18c6b2c50e778ff838e609
Lorenzo Sepulveda <115026126+LorenzoSepulveda@users.noreply.github.com>
2023-06-23 16:50:24 -0300

    Update CEducacion.java

--- src/main/java/com/portfolio/ls/Controller/CEducacion.java
+++ src/main/java/com/portfolio/ls/Controller/CEducacion.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app/")
+@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;

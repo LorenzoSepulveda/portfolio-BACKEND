68e95be93e6c94dbf56eb477b422e4cb33bb6d03
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-06-23 19:06:15 -0300

    crossorigin change

--- src/main/java/com/portfolio/ls/Controller/PersonaController.java
+++ src/main/java/com/portfolio/ls/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app/")
+@CrossOrigin(origins = "https://frontend-prueba-b6123.web.app")
 public class PersonaController {
 
     @Autowired

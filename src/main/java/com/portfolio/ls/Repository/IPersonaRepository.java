1c067700dd07edaa52e8374427c1759c34c1e966
Lorenzo Sepulveda <lorenzosepulveda1@gmail.com>
2023-06-25 19:19:22 -0300

    c

--- src/main/java/com/portfolio/ls/Repository/IPersonaRepository.java
+++ src/main/java/com/portfolio/ls/Repository/IPersonaRepository.java
@@ -4,7 +4,11 @@ import com.portfolio.ls.Entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ *
+ * @author lor_b
+ */
 @Repository
-public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+public interface IPersonaRepository extends JpaRepository<Persona, Integer> {
     
 }
